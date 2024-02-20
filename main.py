95031e6610265619f9b1d5fae6693132798d73d6
Левшенкова Елена <leka022010@mail.ru>
2024-02-20 19:21:03 +0300

    123

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('vggty')
     print('123')
+    print('vggty')
 
 
 # Press the green button in the gutter to run the script.
